48c85c20b167a9af60c310d01f79322a54808caf
Bhaumik <bhaumikpatel4190@gmail.com>
2026-06-16 22:21:00 +0530

    cosmetic changes

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def main():
                 
                 st.markdown("")
                 
-                start_session_button = st.button('Start Session', width="stretch", key="start_session_button")
+                start_session_button = st.button('▶️ Start Session', width="stretch", key="start_session_button")
                 
                 if start_session_button:
                     st.session_state.exercise_type = plan_exercise
@@ -104,9 +104,9 @@ def main():
                 sets = st.session_state.get('target_sets')
                 reps = st.session_state.get('reps_per_set')
                 
-                st.info(f"**{exercise}** -- {sets} sets / {reps} reps")
+                st.info(f"📄 **{exercise}** -- {sets} sets / {reps} reps")
                 
-                end_session_button = st.button("End Session", width='stretch',key='end_session_button')
+                end_session_button = st.button("⏹ End Session", width='stretch',key='end_session_button')
                 
                 if end_session_button:
                     st.session_state.workout_started = False
@@ -134,7 +134,7 @@ def main():
                 sets_completed = st.session_state.get("sets_completed")
                 target_sets = st.session_state.get("target_sets")
                 
-                st.subheader("Progress")
+                st.subheader("📊 Progress")
                 
                 st.metric("Total Reps", f"{total_reps}")
                 st.metric("Current Set Reps", f"{current_set_reps} / {reps_per_set}")
@@ -143,30 +143,30 @@ def main():
                 st.divider()
                 
                 if exercise == "Squats":
-                    st.subheader("Squats Metrics")
+                    st.subheader("📈 Squats Metrics")
                     st.metric("Knee Angle", f"{st.session_state.knee_angle}°")
                     st.metric("back Angle", f"{st.session_state.back_angle}°")
                     st.metric("Depth Status", st.session_state.depth_status)
                     
-                elif exercise == "Push-ups":
+                elif exercise == "📈 Push-ups":
                     st.subheader("Push-up Metrics")
                     st.metric("Elbow Angle", f"{st.session_state.elbow_angle}°")
                     st.metric("Body Alignment", st.session_state.body_alignment)
                     st.metric("Hip Position", st.session_state.hip_status)
                 
-                elif exercise == "Biceps Curls (Dumbbell)":
+                elif exercise == "📈 Biceps Curls (Dumbbell)":
                     st.subheader("Curl Metrics")
                     st.metric("Elbow Angle", f"{st.session_state.elbow_angle}°")
                     st.metric("Shoulder Stability", st.session_state.shoulder_status)
                     st.metric("Swing Detection", st.session_state.swing_status)
                 
-                elif exercise == "Shoulder Press":
+                elif exercise == "📈 Shoulder Press":
                     st.subheader("Press Metrics")
                     st.metric("Elbow Angle", f"{st.session_state.elbow_angle}°")
                     st.metric("Arm Extension", st.session_state.extension_status)
                     st.metric("Back Arch", st.session_state.back_arch_status)
                     
-                elif exercise == "Lunges":
+                elif exercise == "📈 Lunges":
                     st.subheader("Lunge Metrics")
                     st.metric("Front Knee Angle", f"{st.session_state.front_knee_angle}°")
                     st.metric("Torso Angle", f"{st.session_state.torso_angle}°")
